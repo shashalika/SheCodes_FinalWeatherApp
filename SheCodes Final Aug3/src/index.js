8713fbd9a888f76a99c83dd3302a2dcd81f60447
Shalika-shalika <studiokun@gmail.com>
2020-08-02 18:19:59 +0200

    Fixed Folder structure, added weather condition function

--- SheCodes Final Aug3/src/index.js
+++ SheCodes Final Aug3/src/index.js
@@ -29,3 +29,35 @@ function formatHours(timestamp) {
 
   return `${hours}:${minutes}`;
 }
+
+function showWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+
+  celsiusTemperature = response.data.main.temp;
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  let dateElement = document.querySelector("#current-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
+}
